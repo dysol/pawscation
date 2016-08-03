70a2f15ca5f8ca1dfb611aaf065b2f46cb2f3f72
Dylan Ng <ngxinde@gmail.com>
2016-08-03 14:17:37 +0800

    user signup formed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        flash[:success] = 'Your account has been successfully created.'
+        flash[:info] = 'Please fill in your account information'
+        format.html { redirect_to @user }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -69,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :mobile_number)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :mobile_number)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/help',    to: 'static_pages#help'
 
+  # Dynamic routes (users)
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
   # CRUD routes (RESTful)
-  resources :users
+  resources :users,   except: [ :new ]
 end
